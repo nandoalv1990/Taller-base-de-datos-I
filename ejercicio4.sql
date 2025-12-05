9e32d3348de716f15080e38d5eb804857ce0ea28
Fernando Alvarez <nandoalv1990@gmail.com>
2025-12-05 15:15:59 -0600

    5/12/2025

---
+++ ejercicio4.sql
@@ -0,0 +1,11 @@
+/*
+La universidad está implementando un sistema para administrar los libros, autores, y los préstamos de libros que hacen los alumnos.
+
+Debes crear una base de datos llamada biblioteca que permita:
+Registrar autores y libros.
+Registrar alumnos que piden libros.
+Registrar los préstamos de libros.
+Consultar información combinada usando consultas complejas.
+
+*/
+
